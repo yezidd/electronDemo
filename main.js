e598466f9450154cb2e9a7940cf0c8e67eec0bde
yzdd <3050232357@qq.com>
2019-01-08 12:08:33 +0800

    init

---
+++ main.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow} = require("electron");
+
+const path = require("path");
+
+const url = require("url");
+
+let win;
+
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    const URL = url.format({
+        pathname: path.join(__dirname, "app/index.html"),
+        protocol: 'file',
+        slashes: true
+    });
+
+    win.loadURL(URL);
+
+    win.webContents.openDevTools();
+
+    win.on("close", () => {
+        win = null;
+    })
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if ("darwin" !== process.platform) {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (win == null) {
+        createWindow();
+    }
+})
